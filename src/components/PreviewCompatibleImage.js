a6976831b44dafa1c5c3bf054e1a95fa8129a12b
sebrobpau <37690990+sebrobpau@users.noreply.github.com>
2019-10-22 15:22:48 +0200

    Add support for background images

--- src/components/PreviewCompatibleImage.js
+++ src/components/PreviewCompatibleImage.js
@@ -2,39 +2,85 @@ import React from "react";
 import PropTypes from "prop-types";
 import {createUseStyles} from "react-jss";
 import Img from "gatsby-image";
+import BackgroundImage from "gatsby-background-image";
 
 const useStyles = createUseStyles({
     img: {
         maxWidth: "100%"
-    }
+    },
 });
 
-const PreviewCompatibleImage = ({className, imageInfo}) => {
+const PreviewCompatibleImage = ({className, children, imageInfo, type}) => {
     const classes = useStyles();
     const {alt = "", childImageSharp, image} = imageInfo;
 
     if (!!image && !!image.childImageSharp) {
-        return (
-            <Img
-                className={`${classes.img} ${className}`}
-                fluid={image.childImageSharp.fluid}
-                alt={alt}
-            />
-        );
+        if (type === "background") {
+            return (
+                <BackgroundImage
+                    className={`${classes.img} ${className}`}
+                    fluid={image.childImageSharp.fluid}
+                    alt={alt}
+                >
+                    {children}
+                </BackgroundImage>
+            );
+        } else {
+            return (
+                <Img
+                    className={`${classes.img} ${className}`}
+                    fluid={image.childImageSharp.fluid}
+                    alt={alt}
+                />
+            );
+        }
     }
 
     if (!!childImageSharp) {
-        return (
-            <Img
-                className={`${classes.img} ${className}`}
-                fluid={childImageSharp.fluid}
-                alt={alt}
-            />
-        );
+        if (type === "background") {
+            return (
+                <BackgroundImage
+                    className={`${classes.backgroundImage} ${className}`}
+                    fluid={childImageSharp.fluid}
+                    alt={alt}
+                >
+                    {children}
+                </BackgroundImage>
+            );
+        } else {
+            return (
+                <Img
+                    className={`${classes.img} ${className}`}
+                    fluid={childImageSharp.fluid}
+                    alt={alt}
+                />
+            );
+        }
     }
 
-    if (!!image && typeof image === "string")
-        return <img className={`${classes.img} ${className}`} src={image} alt={alt} />;
+    if (!!image && typeof image === "string") {
+        if (type === "background") {
+            return (
+                <div
+                    className={`${classes.img} ${className}`}
+                    style={{
+                        backgroundImage: `url(${image})`,
+                    }}
+                    alt={alt}
+                >
+                    {children}
+                </div>
+            );
+        } else {
+            return (
+                <img
+                    className={`${classes.img} ${className}`}
+                    src={image}
+                    alt={alt}
+                />
+            );
+        }
+    }
     return null;
 };
 
@@ -43,10 +89,15 @@ PreviewCompatibleImage.propTypes = {
     imageInfo: PropTypes.shape({
         alt: PropTypes.string,
         childImageSharp: PropTypes.object,
-        image: PropTypes.oneOfType([PropTypes.object, PropTypes.string])
-            .isRequired,
+        image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
         style: PropTypes.object
-    }).isRequired
+    }).isRequired,
+    type: PropTypes.string,
+    children: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.element,
+        PropTypes.arrayOf(PropTypes.element)
+    ])
 };
 
 export default PreviewCompatibleImage;
