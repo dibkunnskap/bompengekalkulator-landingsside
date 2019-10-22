34c9a560a3df1b2575402ef35331d1041b5355f4
sebrobpau <37690990+sebrobpau@users.noreply.github.com>
2019-10-22 14:29:22 +0200

    Add support for opening intercom with class=intercom

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,5 +4,10 @@ import theme from "./src/config/theme";
 import "./src/styles/global.css";
 
 export const wrapRootElement = ({element}) => {
+    window.intercomSettings = {
+        app_id: "igto4vfp",
+        hide_default_launcher: true,
+        custom_launcher_selector: ".intercom"
+    };
     return <ThemeProvider theme={theme}>{element}</ThemeProvider>;
 };
